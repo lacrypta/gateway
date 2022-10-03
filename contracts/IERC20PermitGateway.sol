8d390c5ae6a32c5a97fe077150a12464c502e787
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2022-10-03 16:35:38 -0300

    Update license

--- contracts/IERC20PermitGateway.sol
+++ contracts/IERC20PermitGateway.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.17;
 
 import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
