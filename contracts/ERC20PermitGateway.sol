b7620850659118d5e9ce0219e96d27b5b47463e4
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2022-10-05 20:37:22 -0300

    Remove unused import

--- contracts/ERC20PermitGateway.sol
+++ contracts/ERC20PermitGateway.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.17;
 
-import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
